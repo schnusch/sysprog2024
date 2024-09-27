1a880069aad8636936ce8929edec24e4c0247729
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-09-27 16:08:35 +0200

    Implement line reading.

--- getpid/main.cc
+++ getpid/main.cc
@@ -80,17 +80,98 @@ void *malloc(unsigned int size) {
     return res;
 }
 
-int main() {
-    void *test = malloc(4);
+unsigned int strlen(const char *str) {
+    unsigned int len = 0;
+    while(*str++)
+        len++;
+    return len;
+}
 
-    int lines = 0;
-    char c;
-    while(read(0, &c, 1) > 0) {
-        if(c == '\n')
-            lines++;
+void *memcpy(void *dst, const void *src, unsigned int size) {
+    char *d = (char*)dst;
+    char *s = (char*)src;
+    char *end = d + size;
+    while(d != end) {
+        *d = *s;
+        d++;
+        s++;
     }
-    print_num(lines);
+    return dst;
+}
+
+void *realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+    void *new_ptr = malloc(new_size);
+    if(!new_ptr)
+        return NULL;
+    memcpy(new_ptr, ptr, old_size);
+    return new_ptr;
+}
+
+char *read_line(int *eof) {
+    unsigned int cur_size = 16;
+    unsigned int pos = 0;
+    char *buf = (char*)malloc(cur_size);
+    if(!buf)
+        return NULL;
+
+    char c;
+    int res;
+    while((res = read(0, &c, 1)) > 0) {
+        if(c == '\n')
+            break;
+        if(pos + 1 >= cur_size) {
+            unsigned int new_size = cur_size * 2;
+            char *new_buf = (char*)realloc(buf, cur_size, new_size);
+            if(!new_buf)
+                return NULL;
+            cur_size = new_size;
+            buf = new_buf;
+        }
+        buf[pos] = c;
+        pos++;
+    }
+    if(res == 0)
+        *eof = 1;
+    buf[pos] = '\0';
+    return buf;
+}
+
+char **read_lines(unsigned int *count) {
+    unsigned int line_size = 16;
+    char **lines = (char**)malloc(line_size * sizeof(char*));
+    if(!lines)
+        return NULL;
+
+    while(1) {
+        int eof = 0;
+        char *line = read_line(&eof);
+        if(!line)
+            return NULL;
+        if(eof)
+            break;
+        if(*count >= line_size) {
+            unsigned int new_size = line_size * 2;
+            char **new_buf = (char**)realloc(lines, line_size * sizeof(char*), new_size * sizeof(char*));
+            if(!new_buf)
+                return NULL;
+            line_size = new_size;
+            lines = new_buf;
+        }
+        lines[*count] = line;
+        *count += 1;
+    }
+    return lines;
+}
+
+int main() {
+    unsigned int count = 0;
+    char **lines = read_lines(&count);
+    print_num(count);
     write(1, "\n", 1);
+    for(unsigned int i = 0; i < count; ++i) {
+        write(1, lines[i], strlen(lines[i]));
+        write(1, "\n", 1);
+    }
     return 0;
 }
 
