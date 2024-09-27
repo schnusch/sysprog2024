d0ea83a70e1cc806642ca2cf011b486a46283ad7
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-09-27 11:41:36 +0200

    Proper startup.

--- getpid/main.cc
+++ getpid/main.cc
@@ -9,10 +9,26 @@ static int getpid() {
     return v;
 }
 
-/* write a program */
-extern "C" void _start() {
+static unsigned int write(int fd, const char *buffer, unsigned int count) {
+    unsigned int v = SYS_write;
+    asm volatile (
+        "int $0x80"
+        : "+a"(v) : "b"(fd), "c"(buffer), "d"(count)
+    );
+    return v;
+}
+
+extern "C" void exit(int code) {
+    int v = SYS_exit;
+    asm volatile (
+        "int $0x80"
+        :: "a"(v), "b"(code)
+    );
+}
+
+int main() {
     getpid();
-    while(1)
-        ;
+    write(1, "Hello World!\n", 13);
+    return 0;
 }
 
