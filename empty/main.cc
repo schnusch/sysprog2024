8953ca6b2dd7a60374b1d3ba331f1fc4995003b3
schnusch <schnusch@users.noreply.github.com>
2024-09-27 09:54:06 +0200

    avoid SIGSEGV by looping forever

--- empty/main.cc
+++ empty/main.cc
@@ -1,7 +1,3 @@
 extern "C" void _start() {
-	// This will still segfault when `_start` returns. `ret` will pop an address
-	// from the stack and jump there, but since this function was not called
-	// properly, but only jumped into, the stack was not set up properly and
-	// execution after the `ret` will continue at some point, that will cause
-	// a SIGSEGV.
+	while(1) { }
 }
