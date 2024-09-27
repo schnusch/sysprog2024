ee78caf82b0e4fb4dc0975c4df3ae34599d81655
schnusch <schnusch@users.noreply.github.com>
2024-09-27 09:51:35 +0200

    add empty _start()

--- empty/main.cc
+++ empty/main.cc
@@ -1,1 +1,7 @@
-/* write a program */
+extern "C" void _start() {
+	// This will still segfault when `_start` returns. `ret` will pop an address
+	// from the stack and jump there, but since this function was not called
+	// properly, but only jumped into, the stack was not set up properly and
+	// execution after the `ret` will continue at some point, that will cause
+	// a SIGSEGV.
+}
