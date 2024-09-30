67a139608f13a5f44b1f0eb2ddcea895cacafa42
schnusch <schnusch@users.noreply.github.com>
2024-10-01 01:12:36 +0200

    add find_testbed to Docker container (tmpfs instead of image)

--- 2-find/test/entrypoint.sh
+++ 2-find/test/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 PS4='$ '
 set -ex
+sudo tar -C /find_testbed/testbed/basic/MNT --zstd -xpf /find_testbed/xdev.tar.zstd
 sudo chown -R user:users /test
 cp -a /host/* /test
 make -C /test clean all
