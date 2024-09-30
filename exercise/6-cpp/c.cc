15114e16c8a25c31e8bc7d9fe4b0118cb0ec4879
schnusch <schnusch@users.noreply.github.com>
2024-09-30 11:22:35 +0200

    implicit constructor does not memset

--- exercise/6-cpp/c.cc
+++ exercise/6-cpp/c.cc
@@ -2,6 +2,7 @@
 
 class Foo
 {
+public:
   // the single phantom byte is omitted for non-empty classes
   char c;
 };
@@ -13,5 +14,10 @@ int main()
 {
   printf("hello world\n");
   printf("sizeof(o) == %zd\n", sizeof(o));
+  // global variables are in the bss segment, which is zero
+  printf("o.c == %hhd\n", o.c);
+  // objects on stack may be uninitialized
+  Foo l;
+  printf("l.c == %hhd\n", l.c);
   return 0;
 }
